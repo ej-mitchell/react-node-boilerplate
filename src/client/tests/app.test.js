d0c2919f20ef7fd0c80ad7bdf4dfa676ac9c574f
Eliza Mitchell <eliza.j.mitchell@gmail.com>
2018-05-13 11:01:17 -0400

    basic tests for App.js done

--- src/client/tests/app.test.js
+++ src/client/tests/app.test.js
@@ -1,13 +1,25 @@
 import App from '../components/app'
+import Stateless from '../components/stateless'
+import Stateful from '../containers/stateful'
 import React from 'react'
 import {shallow} from 'enzyme'
 
 describe('App', () => {
     test('should match snapshot', () => {
-        const wrapper = shallow(<App/>)
+      const wrapper = shallow(<App/>)
 
-        expect(wrapper.find('h1').text()).toBe('Welcome to React Boilerplate App')
-        expect(wrapper).toMatchSnapshot()
+      expect(wrapper.find('h1').text()).toBe('Welcome to React Boilerplate App')
+      expect(wrapper).toMatchSnapshot()
 
+    })
+
+    test('should contain stateless component', () => {
+      const wrapper = shallow(<App/>)
+      expect(wrapper.find(Stateless).exists())
+    })
+
+    test('should contain stateful component', () => {
+      const wrapper = shallow(<App/>)
+      expect(wrapper.find(Stateful).exists())
     })
 })
